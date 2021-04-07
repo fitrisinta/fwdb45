1001b35efbfff59f47f4b2e7f8553f70c72a0adf
Fitri Sinta <zaricolour@gmail.com>
2021-04-07 21:03:04 +0700

    added pages

--- js/todos.js
+++ js/todos.js
@@ -1,6 +1,6 @@
 const todoForm = document.querySelector('.todo-form');
 const todoInput = document.querySelector('.todo-input');
-const todoItem = document.querySelector('.todo-items');
+const todoItemsList = document.querySelector('.todo-items');
 
 let todos = [];
 
@@ -12,16 +12,93 @@ todoForm.addEventListener('submit', function (event) {
 function addTodo(item) {
     if (item !== '') {
         const todo = {
-            id = Date.now(),
+            id: Date.now(),
             name: item,
             completed: false
         };
 
         todos.push(todo);
-        renderTodos(todos);
+        addToLocalStorage(todos);
 
         todoInput.value = '';
     }
 }
 
+function renderTodos(todos) {
+    todoItemsList.innerHTML = '';
 
+    todos.forEach(function (item) {
+
+        const checked = item.completed ? 'checked' : null;
+
+        const li = document.createElement('li');
+        li.setAttribute('class', 'item mt-2 mx-auto d-flex align-items-center');
+        li.setAttribute('data-key', item.id);
+
+        if (item.completed === true) {
+            li.classList.add('checked');
+        }
+
+        li.innerHTML = `
+        <input type="checkbox" class="checkbox" ${checked}>
+        ${item.name}
+        <button class="ml-auto delete-button btn btn-sm btn-danger">Delete</button>
+      `;
+
+        todoItemsList.append(li);
+    });
+
+}
+
+
+function addToLocalStorage(todos) {
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    renderTodos(todos);
+}
+
+
+function getFromLocalStorage() {
+    const reference = localStorage.getItem('todos');
+
+    if (reference) {
+        todos = JSON.parse(reference);
+        renderTodos(todos);
+    }
+}
+
+
+function toggle(id) {
+    todos.forEach(function (item) {
+        if (item.id == id) {
+            item.completed = !item.completed;
+        }
+    });
+
+    addToLocalStorage(todos);
+}
+
+
+function deleteTodo(id) {
+    todos = todos.filter(function (item) {
+        return item.id != id;
+    });
+
+
+    addToLocalStorage(todos);
+}
+
+
+getFromLocalStorage();
+
+
+todoItemsList.addEventListener('click', function (event) {
+    if (event.target.type === 'checkbox') {
+        toggle(event.target.parentElement.getAttribute('data-key'));
+    }
+
+
+    if (event.target.classList.contains('delete-button')) {
+        deleteTodo(event.target.parentElement.getAttribute('data-key'));
+    }
+});
